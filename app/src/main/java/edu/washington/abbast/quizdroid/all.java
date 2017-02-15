c4cd1664f2275d980f0f9a068fefdee10883b991
Abbas Tahirzadeh <Abbas@Abbass-MacBook-Air.local>
2017-02-15 03:03:20 -0800

    Cleaned up code and bugs

---
+++ app/src/main/java/edu/washington/abbast/quizdroid/all.java
@@ -0,0 +1,13 @@
+package edu.washington.abbast.quizdroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class all extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all);
+    }
+}
